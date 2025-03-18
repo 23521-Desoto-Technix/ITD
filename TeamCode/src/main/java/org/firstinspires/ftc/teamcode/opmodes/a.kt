0f6318b45c0295801346b09e8bd4d71fde33ceac
nathan ricker <142940920+dragonher0@users.noreply.github.com>
2025-03-18 18:26:00 -0500

    hey

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/a.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/a.kt
@@ -130,7 +130,7 @@ class v2 : LinearOpMode() {
             val denominator = max(abs(rotY) + abs(rotX) + abs(rx), 1.0)
             var mult = 1.0
             if (gamepad1.right_bumper) {
-                mult = 0.3
+                mult = 0.35
                 frontLeft.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
                 backLeft.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
                 frontRight.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
